5b11e1db444aa70272716d2fcc58311ddad977b2
SANDESHPC <SANDESHPC@DESKTOP-NQ70LMC>
2022-03-17 13:41:26 +0530

    Add project files.

---
+++ JQueryAjaxMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,61 @@
+﻿using JQueryAjaxMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JQueryAjaxMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        DbMVCEntities Db = new DbMVCEntities();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployees()); 
+        }
+
+        IEnumerable<Employee> GetAllEmployees()
+        {
+            return Db.Employees.ToList<Employee>();
+        }
+
+        public ActionResult AddOrEdit(int id = 0)
+        {  Employee emp = new Employee();
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee emp)
+        {
+            try
+            {
+                if (emp.ImageUpload != null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(emp.ImageUpload.FileName);
+                    string extension = Path.GetExtension(emp.ImageUpload.FileName);
+                    fileName = fileName + DateTime.Now.ToString("yymmssffff") + extension;
+                    emp.ImagePath = "~/AppFiles/Images/" + fileName;
+                    emp.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/AppFiles/Images/"), fileName));
+                }
+                Db.Employees.Add(emp);
+                Db.SaveChanges();
+
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployees()), message = "Submitted Successfully" }, JsonRequestBehavior.AllowGet);
+           
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+
+            }
+        }
+    }
+}
---
+++ JQueryAjaxMVC/Scripts/Scripts.js
@@ -0,0 +1,52 @@
+﻿const { ajax } = require("jquery");
+
+function ShowImagePreview(imageUploader, previewImage) {
+    if (imageUploader.files && imageUploader.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(previewImage).attr('src', e.target.result);
+        }
+        reader.readAsDataURL(imageUploader.files[0]);
+    }
+}
+
+function jQueryAjaxPost(form) {
+    $.validator.unobtrusive.parse(form);
+    if ($(form).valid()) {
+        var ajaxConfig = {
+            type: 'POST',
+            url: form.action,
+            success: function (response) {
+                if (response.success) {
+                    $("#firstTab").html(response.html);
+                    refreshAddNewTab($(form).attr('data-resetUrl'), true)
+                    //Success 
+                    $.notify(response.message,"success")
+                }
+                else {
+                    //error
+                    $.notify(response.message,"error")
+                }
+            }
+
+        }
+        if ($(form).attr('enctype') == "multipart/form-data") {
+            ajaxConfig["contentType"] = false;
+            ajaxConfig["processData"] = false;
+        }
+        $.ajax(ajaxConfig);
+    }
+}
+
+function refreshAddNewTab(resetUrl, showViewTab) {
+    $.ajax({
+        type: 'GET',
+        url: resetUrl,
+        success: function (response) {
+            $("#secondTab").html(response);
+            $('ul.nav.nav-tabs a:eq(1)').html("Add New");
+            if (showViewTab)
+                $('ul.nav.nav-tabs a:eq(0)').tab('show');
+        }
+    });
+}
